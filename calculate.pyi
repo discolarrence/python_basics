b697b8f3af9d5720c199220f32a6b8839238ff42
discolarrence <discolarrence@gmail.com>
2021-07-09 20:45:18 -0400

    added decorators, calculate

---
+++ calculate.pyi
@@ -0,0 +1,9 @@
+from numbers import Real
+
+def add(num1: Real, num2: Real) -> Real
+
+def subtract(num1: Real, num2: Real) -> Real
+
+def multiply(num1: Real, num2: Real) -> Real
+
+def divide(num1: Real, num2: Real) -> Real
